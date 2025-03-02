1a30d38c2d00fd8e5c36090e411b462a8ea107bc
kira217-cyber <gamingkira217@gmail.com>
2025-03-02 11:30:22 +0600

    complete next step

--- script/toggle.js
+++ script/toggle.js
@@ -1,6 +1,6 @@
 document.getElementById("btn-other-page").addEventListener("click",function()
 {
-    window.location.href="http://127.0.0.1:5500//Blogs/index.html"
+    window.location.href="/Blogs/index.html"
 })
 
 
