9fbd957b9c1e0a7441b62606993d857618807a9d
kira217-cyber <gamingkira217@gmail.com>
2025-03-02 11:38:57 +0600

    complete all part

--- script/home.js
+++ script/home.js
@@ -1,3 +1,3 @@
 document.getElementById("btn-home-page").addEventListener('click',function(){
-    window.location.href="http://127.0.0.1:5500/index.html"
+    window.location.href="./index.html"
 })
